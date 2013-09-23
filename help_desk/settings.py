de885c023adb54287d84a107440e669a21fdbe12
Donatas <donce.lt@gmail.com>
2013-09-23 21:04:48 +0300

    Django admin, docs (/admin, /admin/doc/)

--- help_desk/settings.py
+++ help_desk/settings.py
@@ -122,10 +122,8 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
 	'django_extensions',
 	'help_desk',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
