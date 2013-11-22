16355a89ed9ccb2ff56b0f2b58e0cfbf3bfb1756
Donatas <donce.lt@gmail.com>
2013-11-22 20:04:49 +0200

    pep8

--- help_desk/management/commands/createdata.py
+++ help_desk/management/commands/createdata.py
@@ -1,8 +1,9 @@
 #encoding: utf-8
 from django.core.management.base import BaseCommand
 
-from help_desk.models import BaseUser, ROLE_MANAGER, Client, Employee, Request,\
-Service, REQUEST_TYPE_REQUEST
+from help_desk.models import BaseUser, ROLE_MANAGER, Client, Employee, Request, \
+    Service, REQUEST_TYPE_REQUEST
+
 
 class Command(BaseCommand):
     help = 'Creates sample data.'
@@ -17,10 +18,13 @@ class Command(BaseCommand):
         print 'Sample employee account: admin admin'
         print 'Sample client account: client client'
         client = BaseUser.objects.create_client('client', 'client', 'UAB "Įmonė"', "Imonės g, Vilnius, Lietuva")
-        employee = BaseUser.objects.create_employee('admin', 'admin', 'Petras', 'Petraitis', ROLE_MANAGER, 'petras@petraitis.lt', '865432100')
-        employeeB = BaseUser.objects.create_employee('admin2', 'admin2', 'Antanas', 'Antanaitis', ROLE_MANAGER, 'antanas@antanaitis.lt', '865432101')
+        employee = BaseUser.objects.create_employee('admin', 'admin', 'Petras', 'Petraitis', ROLE_MANAGER,
+                                                    'petras@petraitis.lt', '865432100')
+        employeeB = BaseUser.objects.create_employee('admin2', 'admin2', 'Antanas', 'Antanaitis', ROLE_MANAGER,
+                                                     'antanas@antanaitis.lt', '865432101')
 
         service = Service.objects.create(description='Serverių hostingas', limit_inc=5, limit_req=2)
-        request = Request.objects.create(type=REQUEST_TYPE_REQUEST, client=client, receive_type='phone', service=service)
+        request = Request.objects.create(type=REQUEST_TYPE_REQUEST, client=client, receive_type='phone',
+                                         service=service)
         request.assign(employeeB)
         request.assign(employee)
