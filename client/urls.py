82c13f644900bf7c4befc21572060d7b05c69fb7
Donatas <donce.lt@gmail.com>
2013-12-01 18:36:04 +0400

    client structure

--- client/urls.py
+++ client/urls.py
@@ -2,4 +2,8 @@ from django.conf.urls import patterns, url
 
 client_patterns = patterns('client.views',
                            url(r'^$', 'home'),
+                           url(r'^issues/$', 'issues'),
+                           url(r'services/$', 'services'),
+                           url(r'contracts/$', 'contracts'),
+                           url(r'information/$', 'information'),
 )
--- client/views.py
+++ client/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'client/home.html', {})
+
+
+def issues(request):
+    return render(request, 'client/home.html', {})
+
+
+def services(request):
+    return render(request, 'client/home.html', {})
+
+
+def contracts(request):
+    return render(request, 'client/home.html', {})
+
+
+def information(request):
+    return render(request, 'client/home.html', {})
