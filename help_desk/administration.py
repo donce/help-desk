39cbf03d3e80b10f4a810ad59de24e132cf19824
Karolis Jocevičius <karolis.jocevicius@gamil.com>
2013-12-07 21:10:07 +0200

    some import stuff

--- help_desk/administration.py
+++ help_desk/administration.py
@@ -27,6 +27,9 @@ class XLSXImporter:
         sheet_sut_pasl = book.sheet_by_name("SutPasl")
         sheet_kreipiniai = book.sheet_by_name("Kreipiniai")
         sheet_paskyrimai = book.sheet_by_name("Paskyrimai")
+        
+        parse_paslaugos(sheet_paslaugos)
+        parse_darbuotojai(sheet_darbuotojai)
 
 
     def parse_paslaugos(self, sheet):
@@ -66,12 +69,48 @@ class XLSXImporter:
             client.save()
             
     def parseAtstovai(self, sheet):
-        client = sheet.cell(i,2)
-        first_name = sheet.cell(i,3)
-        last_name = sheet.cell(i,4)
-        phone_number = sheet.cell(i,5)
-        email = sheet.cell(i,6)
-        active = sheet.cell(i,7)
+        for i in range(sheet.nrows):
+            client = sheet.cell(i,2)
+            first_name = sheet.cell(i,3)
+            last_name = sheet.cell(i,4)
+            phone_number = sheet.cell(i,5)
+            email = sheet.cell(i,6)
+            active = sheet.cell(i,7) #boolean?
+            delegate = Delegate(client = client, first_name=first_name, last_name=last_name, phone_number=phone_number, email=email, active=active)
+            delegate.save()
+            
+    def parseSutartys(self, sheet):
+        for i in range(sheet.nrows):
+            id = sheet.cell(i,1)
+            number = sheet.cell(i,2)
+            title = sheet.cell(i,3)
+            client = sheet.cell(i,4)
+            start = sheet.cell(i,5)
+            end = sheet.cell(i,6)
+            contract = Contract(id=id, number=number, title=title, client=client, start=start, end=end)
+            contract.save()
+        
+    def parseKreipiniai(self, sheet):
+        for i in range(sheet.nrows):
+            id = sheet.cell(i,1)
+            client = sheet.cell(i,2)
+            service = sheet.cell(i,3)
+            type = sheet.cell(i,4)
+            receive_type = sheet.cell(i,5)
+            title = sheet.cell(i,6)
+            description = sheet.cell(i,7)
+            created = sheet.cell(i,8)
+            closed = sheet.cell(i,9)
+            status = sheet.cell(i,10)
+            rating = sheet.cell(i,11)
+            current = sheet.cell(i,12)
+            previous = sheet.cell(i,13) #TODO: purpose of this?
+            issue = Issue(id=id, client=client, service=service, type=type, receive_type=receive type, title=title, description=description, created=created, closed=closed, status=status, rating=rating, current=current, previous=previous)
+            issue.save()
             
             
+        
+        
+        
+         
             
