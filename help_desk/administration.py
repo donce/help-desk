15eea90edc115715f3ec6385362b55e271f2c21f
Donatas <donce.lt@gmail.com>
2013-12-12 21:48:13 +0200

    python errors, pep8

--- help_desk/administration.py
+++ help_desk/administration.py
@@ -1,4 +1,5 @@
-from xlrd import open_workbook, sheet
+from xlrd import open_workbook
+from help_desk.models import Service, ROLE_ENGINEER, ROLE_MANAGER, ROLE_ADMINISTRATOR, Employee, Client, Issue, Contract, Delegate
 import models
 
 
@@ -27,9 +28,9 @@ class XLSXImporter:
         sheet_sut_pasl = book.sheet_by_name("SutPasl")
         sheet_kreipiniai = book.sheet_by_name("Kreipiniai")
         sheet_paskyrimai = book.sheet_by_name("Paskyrimai")
-        
-        parse_paslaugos(sheet_paslaugos)
-        parse_darbuotojai(sheet_darbuotojai)
+
+        self.parse_paslaugos(sheet_paslaugos)
+        self.parse_darbuotojai(sheet_darbuotojai)
 
 
     def parse_paslaugos(self, sheet):
@@ -39,77 +40,78 @@ class XLSXImporter:
             limit_req = sheet.cell(i, 4)
             service = Service(description=description, limit_inc=limit_inc, limit_req=limit_req)
             service.save()
-            
+
     def parseDarbuotojai(self, sheet):
         for i in range(sheet.nrows):
-            first_name = sheet.cell(i,2)
-            last_name = sheet.cell(i,3)
-            role = sheet.cell(i,4)
-            phone_number = sheet.cell(i,5)
-            email = sheet.cell(i,6)
-            
+            first_name = sheet.cell(i, 2)
+            last_name = sheet.cell(i, 3)
+            role = sheet.cell(i, 4)
+            phone_number = sheet.cell(i, 5)
+            email = sheet.cell(i, 6)
+
             if role == 'I':
                 role = ROLE_ENGINEER
             elif role == 'V':
                 role = ROLE_MANAGER
             elif role == 'A':
                 role = ROLE_ADMINISTRATOR
-            
+
             # TODO : user ???
             employee = Employee(first_name=first_name, last_name=last_name, role=role, phone_number=phone_number, email=email)
             employee.save()
-            
+
     def parseKlientai(self, sheet):
         for i in range(sheet.nrows):
-            title = sheet.cell(i,2)
-            address = sheet.cell(i,3)
-            
+            title = sheet.cell(i, 2)
+            address = sheet.cell(i, 3)
+
             # TODO : user ???
             client = Client(title=title, address=address)
             client.save()
-            
+
     def parseAtstovai(self, sheet):
         for i in range(sheet.nrows):
-            client = Client.get(id=trimID(sheet.cell(i,2), "K"))
-            first_name = sheet.cell(i,3)
-            last_name = sheet.cell(i,4)
-            phone_number = sheet.cell(i,5)
-            email = sheet.cell(i,6)
+            client = Client.get(id=self.trimID(sheet.cell(i, 2), "K"))
+            first_name = sheet.cell(i, 3)
+            last_name = sheet.cell(i, 4)
+            phone_number = sheet.cell(i, 5)
+            email = sheet.cell(i, 6)
             #active = sheet.cell(i,7) #boolean?
             active = True
-            delegate = Delegate(client = client, first_name=first_name, last_name=last_name, phone_number=phone_number, email=email, active=active)
+            delegate = Delegate(client=client, first_name=first_name, last_name=last_name, phone_number=phone_number, email=email, active=active)
             delegate.save()
-            
+
     def parseSutartys(self, sheet):
         for i in range(sheet.nrows):
-            id = sheet.cell(i,1)
-            number = sheet.cell(i,2)
-            title = sheet.cell(i,3)
-            client = Client.get(id=trimID(sheet.cell(i,4), "K"))
-            start = sheet.cell(i,5)
-            end = sheet.cell(i,6)
+            id = sheet.cell(i, 1)
+            number = sheet.cell(i, 2)
+            title = sheet.cell(i, 3)
+            client = Client.get(id=self.trimID(sheet.cell(i, 4), "K"))
+            start = sheet.cell(i, 5)
+            end = sheet.cell(i, 6)
             contract = Contract(id=id, number=number, title=title, client=client, start=start, end=end)
             contract.save()
-        
+
     def parseKreipiniai(self, sheet):
         for i in range(sheet.nrows):
-            id = sheet.cell(i,1)
-            client = Client.get(id=trimID(sheet.cell(i,2), "K"))
-            service = trimID(sheet.cell(i,3), "P")
-            type = sheet.cell(i,4)
-            receive_type = sheet.cell(i,5)
-            title = sheet.cell(i,6)
-            description = sheet.cell(i,7)
-            created = sheet.cell(i,8)
-            closed = sheet.cell(i,9)
-            status = sheet.cell(i,10)
-            rating = sheet.cell(i,11)
-            current = sheet.cell(i,12)
-            previous = sheet.cell(i,13) #TODO: purpose of this?
-            issue = Issue(id=id, client=client, service=service, type=type, receive_type=receive_type, title=title, description=description, created=created, closed=closed, status=status, rating=rating, current=current, previous=previous)
+            id = sheet.cell(i, 1)
+            client = Client.get(id=self.trimID(sheet.cell(i, 2), "K"))
+            service = self.trimID(sheet.cell(i, 3), "P")
+            type = sheet.cell(i, 4)
+            receive_type = sheet.cell(i, 5)
+            title = sheet.cell(i, 6)
+            description = sheet.cell(i, 7)
+            created = sheet.cell(i, 8)
+            closed = sheet.cell(i, 9)
+            status = sheet.cell(i, 10)
+            rating = sheet.cell(i, 11)
+            current = sheet.cell(i, 12)
+            previous = sheet.cell(i, 13) #TODO: purpose of this?
+            issue = Issue(id=id, client=client, service=service, type=type, receive_type=receive_type, title=title, description=description, created=created, closed=closed, status=status,
+                          rating=rating, current=current, previous=previous)
             issue.save()
-    
-            
+
+
     def trimID(self, id, prefix):
         return int(id.lstrip(prefix))
         
