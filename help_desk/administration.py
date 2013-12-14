ae6afa2f2c097545c9b0e250d147e84313a0fd20
Karolis Jocevičius <karolis.jocevicius@gamil.com>
2013-12-14 21:29:43 +0200

    Import - reikia sutvarkyti datos konvertinima is tuple ir prideti userius

--- help_desk/administration.py
+++ help_desk/administration.py
@@ -1,6 +1,8 @@
 from xlrd import open_workbook
 from help_desk.models import Service, ROLE_ENGINEER, ROLE_MANAGER, ROLE_ADMINISTRATOR, Employee, Client, Issue, Contract, Delegate
 import models
+import xlrd
+import datetime
 
 
 def clean_database():
@@ -29,25 +31,32 @@ class XLSXImporter:
         sheet_kreipiniai = book.sheet_by_name("Kreipiniai")
         sheet_paskyrimai = book.sheet_by_name("Paskyrimai")
 
-        self.parse_paslaugos(sheet_paslaugos)
-        self.parse_darbuotojai(sheet_darbuotojai)
-
-
-    def parse_paslaugos(self, sheet):
-        for i in range(sheet.nrows):
-            description = sheet.cell(i, 2)
-            limit_inc = sheet.cell(i, 3)
-            limit_req = sheet.cell(i, 4)
-            service = Service(description=description, limit_inc=limit_inc, limit_req=limit_req)
+        self.parsePaslaugos(sheet_paslaugos)
+        #self.parseDarbuotojai(sheet_darbuotojai) # TODO : user
+        self.parseKlientai(sheet_klientai)
+        #self.parseAtstovai(sheet_atstovai) # TODO : user
+        #self.parseSutartys(sheet_sutartys, book) # FIXME : tuple to date
+        #self.parseKreipiniai(sheet_kreipiniai, book) # FIXME : date tuple problem
+        #self.parsePaskyrimai(sheet, book) # FIXME : tuple to date
+        #self.parseSutPasl(sheet_sut_pasl)
+        
+    def parsePaslaugos(self, sheet):
+        for i in range(1, sheet.nrows):
+            id = self.trimID(sheet.cell(i, 0).value, "P")
+            description = sheet.cell(i, 1).value
+            limit_inc = sheet.cell(i, 2).value
+            limit_req = sheet.cell(i, 3).value
+            service = Service(id=id, description=description, limit_inc=limit_inc, limit_req=limit_req)
             service.save()
 
     def parseDarbuotojai(self, sheet):
-        for i in range(sheet.nrows):
-            first_name = sheet.cell(i, 2)
-            last_name = sheet.cell(i, 3)
-            role = sheet.cell(i, 4)
-            phone_number = sheet.cell(i, 5)
-            email = sheet.cell(i, 6)
+        for i in range(1, sheet.nrows):
+            id = int(sheet.cell(i, 0).value)
+            first_name = sheet.cell(i, 1).value
+            last_name = sheet.cell(i, 2).value
+            role = sheet.cell(i, 3).value
+            phone_number = sheet.cell(i, 4).value
+            email = sheet.cell(i, 5).value
 
             if role == 'I':
                 role = ROLE_ENGINEER
@@ -57,61 +66,99 @@ class XLSXImporter:
                 role = ROLE_ADMINISTRATOR
 
             # TODO : user ???
-            employee = Employee(first_name=first_name, last_name=last_name, role=role, phone_number=phone_number, email=email)
+            employee = Employee(id=id, first_name=first_name, last_name=last_name, role=role, phone_number=phone_number, email=email)
             employee.save()
 
     def parseKlientai(self, sheet):
-        for i in range(sheet.nrows):
-            title = sheet.cell(i, 2)
-            address = sheet.cell(i, 3)
+        for i in range(1, sheet.nrows):
+            id = int(self.trimID(sheet.cell(i, 0).value, "K"))
+            title = sheet.cell(i, 1).value
+            address = sheet.cell(i, 2).value
 
-            # TODO : user ???
-            client = Client(title=title, address=address)
+            client = Client(id=id, title=title, address=address)
             client.save()
 
     def parseAtstovai(self, sheet):
-        for i in range(sheet.nrows):
-            client = Client.get(id=self.trimID(sheet.cell(i, 2), "K"))
-            first_name = sheet.cell(i, 3)
-            last_name = sheet.cell(i, 4)
-            phone_number = sheet.cell(i, 5)
-            email = sheet.cell(i, 6)
+        for i in range(1, sheet.nrows):
+            id = int(sheet.cell(i, 0).value)
+            client = Client.objects.get(id=int(self.trimID(sheet.cell(i, 1).value, "K")))
+            first_name = sheet.cell(i, 2).value
+            last_name = sheet.cell(i, 3).value
+            phone_number = sheet.cell(i, 4).value
+            email = sheet.cell(i, 5).value
             #active = sheet.cell(i,7) #boolean?
             active = True
+            
+            # TODO : user ???
             delegate = Delegate(client=client, first_name=first_name, last_name=last_name, phone_number=phone_number, email=email, active=active)
             delegate.save()
 
-    def parseSutartys(self, sheet):
-        for i in range(sheet.nrows):
-            id = sheet.cell(i, 1)
-            number = sheet.cell(i, 2)
-            title = sheet.cell(i, 3)
-            client = Client.get(id=self.trimID(sheet.cell(i, 4), "K"))
-            start = sheet.cell(i, 5)
-            end = sheet.cell(i, 6)
+    def parseSutartys(self, sheet, workbook):
+        for i in range(1, sheet.nrows):
+            id = int(sheet.cell(i, 0).value)
+            number = sheet.cell(i, 1).value
+            title = sheet.cell(i, 2).value
+            client = Client.objects.get(id=int(self.trimID(sheet.cell(i, 3).value, "K")))
+            
+            # FIXME : date tuple problem
+            startTuple = xlrd.xldate_as_tuple(sheet.cell(i, 4).value, workbook.datemode)
+            start = datetime.datetime(*startTuple)
+            endTuple = xlrd.xldate_as_tuple(sheet.cell(i, 5).value, workbook.datemode)
+            end = datetime.datetime(*endTuple)
+            
             contract = Contract(id=id, number=number, title=title, client=client, start=start, end=end)
             contract.save()
 
-    def parseKreipiniai(self, sheet):
-        for i in range(sheet.nrows):
-            id = sheet.cell(i, 1)
-            client = Client.get(id=self.trimID(sheet.cell(i, 2), "K"))
-            service = Service.get(id=self.trimID(sheet.cell(i, 3), "P"))
-            type = sheet.cell(i, 4)
-            receive_type = sheet.cell(i, 5)
-            title = sheet.cell(i, 6)
-            description = sheet.cell(i, 7)
-            created = sheet.cell(i, 8)
-            closed = sheet.cell(i, 9)
-            status = sheet.cell(i, 10)
-            rating = sheet.cell(i, 11)
-            current = sheet.cell(i, 12)
-            previous = sheet.cell(i, 13) #TODO: purpose of this?
-            issue = Issue(id=id, client=client, service=service, type=type, receive_type=receive_type, title=title, description=description, created=created, closed=closed, status=status,
-                          rating=rating, current=current, previous=previous)
+    def parseKreipiniai(self, sheet, workbook):
+        for i in range(1, sheet.nrows):
+            id = int(sheet.cell(i, 0).value)
+            client = Client.objects.get(id=int(self.trimID(sheet.cell(i, 1).value, "K")))
+            service = Service.objects.get(id=int(self.trimID(sheet.cell(i, 2).value, "P")))
+            type = sheet.cell(i, 3).value
+            receive_type = sheet.cell(i, 4).value
+            #title = sheet.cell(i, 5).value
+            description = sheet.cell(i, 5).value
+            
+            # FIXME : date tuple problem
+            createdTuple = xlrd.xldate_as_tuple(sheet.cell(i, 6).value, workbook.datemode)
+            created = datetime.datetime(*createdTuple)
+            closedTuple = xlrd.xldate_as_tuple(sheet.cell(i, 7).value, workbook.datemode)
+            closed = datetime.datetime(*closedTuple)
+            
+            #status = sheet.cell(i, 8).value
+            rating = int(sheet.cell(i, 9).value)
+            #current = sheet.cell(i, 10).value # TODO : what?
+            #previous = sheet.cell(i, 11).value # TODO : purpose of this?
+            issue = Issue(id=id, client=client, service=service, type=type,
+                          receive_type=receive_type, description=description, 
+                          created=created, closed=closed, rating=rating)
             issue.save()
 
+    def parsePaskyrimai(self, sheet, workbook):
+        for i in range(1, sheet.nrows):
+            id = int(sheet.cell(i, 0).value)
+            issue = Issue.objects.get(id=int(sheet.cell(i, 1).value))
+            assigned = Employee.objects.get(id=int(sheet.cell(i, 2).value))
+            worker = Employee.objects.get(id=int(sheet.cell(i, 3).value))
+            
+            # FIXME : date tuple problem
+            startTuple = xlrd.xldate_as_tuple(sheet.cell(i, 4).value, workbook.datemode)
+            start = datetime.datetime(*startTuple)
+            endTuple = xlrd.xldate_as_tuple(sheet.cell(i, 5).value, workbook.datemode)
+            end = datetime.datetime(*endTuple)
+            
+            text = sheet.cell(i, 6).value
+            time = int(sheet.cell(i, 8).value)
+            
+            assignment = Assignment(id=id, issue=issue, assigned=assigned,
+                        worker=worker, start=start, end=end, text=text, time=time)
 
+    def parseSutPasl(self, sheet):
+        for i in range(1, sheet.nrows):
+            contract = Contract.objects.get(id=int(sheet.cell(i, 0).value))
+            service = Service.objects.get(id=int(self.trimID(sheet.cell(i, 1).value, "P")))
+            contract.services.add(service)
+            
     def trimID(self, id, prefix):
         return int(id.lstrip(prefix))
         
