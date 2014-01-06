60270a2417c23bac86edd76c5d1b9119941757cc
Rytis <rytis@elektromotus.lt>
2014-01-06 02:29:58 +0200

    date time format fixed

--- help_desk/templatetags/issues_table.py
+++ help_desk/templatetags/issues_table.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db import models
 
 
 register = template.Library()
@@ -13,7 +14,11 @@ def issues_table(model, fields, objects, edit=None):
             values.append(getattr(object, field[0]))
             if values[-1] == None:
                 values[-1] = '-'
-                #TODO: implement Foreign key recognition
+
+            if values[-1].__class__.__name__ == 'datetime':
+                values[-1] = values[-1].strftime("%Y-%m-%d %H:%M:%S")
+                
+            #TODO: implement Foreign key recognition
 
         if len(values) > 0:
             values[0] = u'<a href="{0}">{1}</a>'.format(object.get_absolute_url(edit), values[0])
